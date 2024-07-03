ab2359e9b0a95528411f2917d93529dc9311ada5
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-07-02 22:14:59 -0300

    fix: adicionado menu

--- nelore-na-net/src/Shared/Components/Menu/style.js
+++ nelore-na-net/src/Shared/Components/Menu/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ContainerMenu = styled.nav`
   background-color: #f5f5f5;
@@ -11,7 +11,52 @@ const ContentMenu = styled.div`
   padding: 10px 20px;
 `;
 
-export{
-    ContainerMenu,
-    ContentMenu
-}
+const LogoNeloreNaNet = styled.img`
+  height: 40px;
+`;
+
+const ButtonContainer = styled.div`
+  display: block;
+  button {
+    background-color: #333;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 20px;
+    span{
+      background-color: #fff;
+      display: block;
+      height: 2px;
+      width: 25px;
+    }
+  }
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const ContainerLinks = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
+const ListMenu = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export {
+  ButtonContainer,
+  ContainerMenu,
+  ContentMenu,
+  LogoNeloreNaNet,
+  ContainerLinks,
+  ListMenu,
+};
